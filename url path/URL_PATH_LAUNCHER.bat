1995614e24e0ec1c704f717a09a1bad8a162c3e0
FrostBlack <130504630+WebFrostBlack@users.noreply.github.com>
2023-05-21 19:48:50 +0200

    init

---
+++ url path/URL_PATH_LAUNCHER.bat
@@ -0,0 +1,2 @@
+python urlPath.py
+pause
---
+++ url path/urlPath.py
@@ -0,0 +1,73 @@
+import os
+import requests
+from concurrent.futures import ThreadPoolExecutor
+from termcolor import colored
+
+valid_urls = []
+
+def show_txt_files():
+    base_path = os.path.dirname(os.path.abspath(__file__))
+    txt_dir = os.path.join(base_path, 'txt')
+    files = [f for f in os.listdir(txt_dir) if os.path.isfile(os.path.join(txt_dir, f)) and f.endswith('.txt')]
+    print('Fichiers .txt disponibles :')
+    for i, f in enumerate(files):
+        print(f'[{i+1}] {f}')
+    return txt_dir, files
+
+def test_url(url, path):
+    full_url = f'{url}/{path.strip()}'
+    try:
+        response = requests.get(full_url)
+        if response.status_code == 200:
+            print(colored('Success [' + str(response.status_code) + ']: ' + full_url, 'green'))
+            valid_urls.append(full_url)
+        else:
+            print(colored('Error   [' + str(response.status_code) + ']: ' + full_url, 'red'))
+    except:
+        print(colored(f'Error: {full_url}', 'red'))
+
+def test_all_paths(url):
+    chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-?!*'
+    paths = []
+    for i in range(len(chars)):
+        paths.append(chars[i])
+        for j in range(len(chars)):
+            paths.append(f'{chars[i]}{chars[j]}')
+            for k in range(len(chars)):
+                paths.append(f'{chars[i]}{chars[j]}{chars[k]}')
+    with ThreadPoolExecutor(max_workers=20) as executor:
+        for path in paths:
+            executor.submit(test_url, url, path)
+    write_valid_urls(url, valid_urls)
+
+def write_valid_urls(url, valid_urls):
+    base_path = os.path.dirname(os.path.abspath(__file__))
+    valid_dir = os.path.join(base_path, 'valid_urls')
+    if not os.path.exists(valid_dir):
+        os.mkdir(valid_dir)
+    filename = url.split('//')[1].replace('/', '_') + '.txt'
+    file_path = os.path.join(valid_dir, filename)
+    with open(file_path, 'w') as f:
+        for valid_url in valid_urls:
+            f.write(valid_url + '\n')
+    print(f'Successfully saved valid URLs to {file_path}')
+
+if __name__ == '__main__':
+    url = input('Entrez l\'URL à tester : ')
+    if not url.startswith('http://') and not url.startswith('https://'):
+        url = f'https://{url}'
+    option = input("Tester avec un .txt [1] | Tester avec toutes les possibilités [2]\n")
+    if option == '1':
+        txt_dir, files = show_txt_files()
+        file_index = int(input('Entrez le numéro du fichier à utiliser : '))
+        file_path = os.path.join(txt_dir, files[file_index-1])
+        with open(file_path, 'r') as f:
+            words = f.read().split()
+        with ThreadPoolExecutor(max_workers=20) as executor:
+            for word in words:
+                executor.submit(test_url, url, word)
+        write_valid_urls(url, valid_urls)
+    elif option == '2':
+        test_all_paths(url)
+    else:
+        print('Option invalide')
